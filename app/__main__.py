ef6052c2fe1e3b80554128e4bbce507beff5167a
Jan Bucher <jan@jan-bucher.ch>
2018-11-24 10:39:17 +0100

    Basic stuff

--- app/__main__.py
+++ app/__main__.py
@@ -6,15 +6,19 @@ from app.dto.PublicGameState import PublicGameState
 from app.dto.PublicPlayer import PublicPlayer
 from app.dto.ReturnDirections import ReturnDirections
 
-
-@bottle.post('/start')
+bottle.post('/start')
 def start():
-    return "SomeFancyTeamName"
+    return "SoVollFancy"
 
 
 @bottle.post('/chooseAction')
 def move():
     data = PublicGameState(ext_dict=bottle.request.json)
+    data.agent_id
+    # print gamestate to console
+    print("State: ")
+    print(data)
+
     # TODO: Do things with data acces via data
     return ReturnDirections.random()
 
